50739f82d2e670050e58565a609ccd3599e020c5
Christopher chris <christopherchristps@gmail.com>
2019-05-01 19:25:35 +0530

    tree changes

--- src/app/core/app.routing,module.ts
+++ src/app/core/app.routing,module.ts
@@ -2,7 +2,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from '../components/main/home/home.component';
 import { SkillTreeComponent } from '../components/skills/skill-tree/skill-tree.component';
-import {AboutUsComponent}  from '../components/main/about-us/about-us.component';
+import { AboutUsComponent } from '../components/main/about-us/about-us.component';
 // import { SkillsComponent } from '../components/skills/skills.component';
 // import { SkillDetailsComponent } from '../components/skills/skill-details/skill-details.component';
 // import { SkillAddEditComponent } from '../components/skills/skill-add-edit/skill-add-edit.component';
@@ -12,10 +12,10 @@ import {AboutUsComponent}  from '../components/main/about-us/about-us.component'
 // import { ProfileMyStatsComponent } from '../components/profile/profile/profile-my-stats/profile-my-stats.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full'},
-    { path: 'home', component: HomeComponent},
-    { path: 'tree', component: SkillTreeComponent},
-    { path: 'aboutus', component: AboutUsComponent},
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'tree', component: SkillTreeComponent },
+    { path: 'aboutus', component: AboutUsComponent },
     // { path: 'skills/:id', component: SkillsComponent },
     // { path: 'skill-details/:id', component: SkillDetailsComponent },
     // { path: 'skill-add-edit', component: SkillAddEditComponent },
